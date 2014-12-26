ce81b59df738d5f1b1d1725dd8a06132fe7cdb84
shellbye <444854713@qq.com>
2014-12-26 11:31:21 +0800

    init

---
+++ echo.py
@@ -0,0 +1,16 @@
+__author__ = 'shellbye'
+from twisted.internet import protocol, reactor, endpoints
+
+
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
+
+
+endpoints.serverFromString(reactor, "tcp:1234").listen(EchoFactory())
+reactor.run()
