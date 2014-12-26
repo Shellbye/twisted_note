67d2a51de7a25ff16d0ba0005ba1fcd036ad238e
shellbye <444854713@qq.com>
2014-12-26 18:15:31 +0800

    add pure python echo server

--- echo_server/test.py
+++ echo_server/test.py
@@ -11,6 +11,15 @@ def test_echo():
     print "Echo from echo_server:", data
 
 
+def test_echoserver_simple():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost", 50000))
+    s.send("Hello, World!")
+    data = s.recv(1024)
+    s.close()
+    print "Echo from echo_server:", data
+
 if __name__ == "__main__":
     print "i am test"
-    test_echo()
+    # test_echo()
+    test_echoserver_simple()
